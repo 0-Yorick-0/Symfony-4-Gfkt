e2b7000a9e0ac46a2d3d23f9a0d8b92f190929d9
unknown <W2k234ad@WX-OR6189098.ad.francetelecom.fr>
2019-12-04 12:01:22 +0100

    feat: frontend

---
+++ tuto-agence/src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
+use App\Repository\PropertyRepository;
+
+/**
+ * 
+ */
+class HomeController extends AbstractController
+{
+	/**
+	 * @Route("/", name="home")
+	 */
+	public function index(PropertyRepository $repository): Response
+	{
+		$properties = $repository->findLatest();
+
+		return $this->render('pages/home.html.twig', [
+			'properties' => $properties
+		]);
+	}
+}
